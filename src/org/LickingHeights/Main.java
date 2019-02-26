7407a5753adf5e26a2a8e0bfc7eaf674d0741ac3
Owen <owen.trotto.2021@student.lhschools.org>
2019-02-26 08:11:24 -0500

    Got the first three codes done

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -9,20 +9,45 @@ public class Main {
 
         int firstNumber = 1-10 ;
         int secondNumber = 11-20;
+        int thirdNumber = 21-30;
+
+        String Magic;
+
+        Scanner Keyboard;
+
+        Keyboard = new Scanner(System.in);
+        Magic = Keyboard.nextLine();
 
 
 
-        if (firstNumber==1-10); {
-            System.out.println("You will become famous");
 
-        if  (secondNumber>=3);
-            System.out.println("");
+            if (firstNumber==56) {
+                System.out.println("You will become famous");
+                }
+
+                else if (firstNumber<0) {
+                System.out.println("You will find $10");
+            }
+
+            if (secondNumber==24) {
+                System.out.println("You will find true love");
+            }
+
+            else if (secondNumber<=15) {
+                System.out.println("You will die alone");
+            }
+
+            if (thirdNumber==21) {
+                System.out.println("");
+            }
 
 
 
 
 
-        }
+
+
+
 
     }
 }
