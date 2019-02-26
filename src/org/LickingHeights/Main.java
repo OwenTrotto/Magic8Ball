56b8ddfabfbff008ee4782a5658a3e7ca4e5c0db
Owen <owen.trotto.2021@student.lhschools.org>
2019-02-26 07:28:13 -0500

    Started my code

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -1,8 +1,30 @@
 package org.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//
+
+        int firstNumber = 1-10 ;
+        int secondNumber = 11-20;
+
+
+
+        if (firstNumber==1-10); {
+            System.out.println("You will become famous");
+
+        if  (secondNumber>=3);
+            System.out.println("");
+
+
+
+
+
+        }
+
     }
 }
+
+
